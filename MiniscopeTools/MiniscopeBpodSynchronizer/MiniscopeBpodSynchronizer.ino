ca0000e9492a8eb44395b85aa3ea46d7a367a575
LukasOesch <LukasOesch@users.noreply.github.com>
2021-08-11 20:40:30 -0700

    Adapted 'synchronizeMiniscopeToBpod.py' to write to correct folder structure and switch counting on Teensy on and off. CAUTION: The execution cannot be terminated with a keyboard interrupt so far and it has to be stoppedby rebooting Teensy. Adapted 'MiniscopeBpodSynchronizer.ino' to only start counting upon reception of a singnal from python.

--- MiniscopeTools/MiniscopeBpodSynchronizer/MiniscopeBpodSynchronizer.ino
+++ MiniscopeTools/MiniscopeBpodSynchronizer/MiniscopeBpodSynchronizer.ino
@@ -1,40 +1,118 @@
-#include "TimeLib.h"
+/*
+Teensy code to synchronize the miniscope square pulses (each orientation change indicates
+an acquired frame) and the output from a Bpod BNC (set to high at the beginning of 
+each trial). Run syncMiniscopeToBpod.py to generate a synchronization log file
+(.mscopelog). Frames and trial events are recorded as:
+Recording pin, frame/trial number, timestamp (in miliseconds)
 
-#define MINISCOPE 3
-#define BPOD 6
+This program first reset the frame and trial counts when receiving the serial input
+string '1' (= 49). Then incoming frames and trial events will be logged using the
+respective interrupts. At the end when string '0' (=48) arrives the output of the
+will be interrupted.
+LO, 8/5/2021
+*/
+
+#include "TimeLib.h" //To get millis()
+
+#define MINISCOPE 2
+#define BPOD 23
+#define FINDINTACT 11
+#define TESTMINI 10
 
 // Set up the required variables
-volatile double frameNumber = 0;
-volatile double timeStamp = 0;
-volatile bool BpodFlag = 0;
+volatile unsigned long frameNumber = 0; //Counts the number of frames acquired from the miniscope
+volatile unsigned long long timeStamp = 0; //Assigns a timestamp in milisceonds from the start of the
+//acquired frame from the miniscope or the trial event.
+volatile bool trialStartFlag = 0; //Flag for trial start versus stop
+volatile unsigned long trialNumber = 0; //Count trial number
+volatile bool initialized = 0; //Jumps to true once the frames have been reset and the header is sent
+int serialMessage = -1; //Message from the python function to enable counting or stoppining it.
 
+
+////////////////////
+//Checking
+bool frameSimulation = 0; //simulated miniscope frame square wave
+///////////////////
+
+///-----Setup function for interrupts-------------///
 void setup() {
-Serial.begin(9600);
+  Serial.begin(9600);
 
-//Configure pins
- pinMode(MINISCOPE, INPUT);
+  //Configure pins
+  pinMode(MINISCOPE, INPUT); //Set the miniscope sync signal to input
+  pinMode(BPOD, INPUT); //Set the Bpod trial start/stop signal to input
 
-//Set the interrupts
-attachInterrupt(digitalPinToInterrupt(MINISCOPE), miniscopeFrame, CHANGE);
-//Use digitalPinToInterrupt for compatibility with other arduino devices
-}
+ /////////////////////
+ //For testing
+  pinMode(TESTMINI, OUTPUT); //Generate regular TTL pulses
+  ////////////////////
+ 
+  //Set the interrupts
+  attachInterrupt(digitalPinToInterrupt(MINISCOPE), miniscopeFrame, CHANGE);
+  //Use digitalPinToInterrupt for compatibility with other arduino devices
+  attachInterrupt(digitalPinToInterrupt(BPOD), BpodTrialEvent, CHANGE);
+  }
 
-
+///--------Leave empty for experiment, uncomment for checking------///
 void loop() {
-//Serial.write('1');
-//delay(500);
+  /////////////////////////////
+  //Testing 
+  /*frameSimulation = digitalRead(MINISCOPE);
+  Serial.write("Miniscope pulse state is: ");
+  Serial.println(frameSimulation); 
+  */
+  //digitalWrite(TESTMINI, HIGH);
+  //delay(1000);
+  /*
+  frameSimulation = digitalRead(MINISCOPE);
+  Serial.write("Miniscope pulse state is: ");
+  Serial.println(frameSimulation);
+
+  Serial.write("Bpod state is: ");
+  Serial.println(frameSimulation);
+  */
+  //digitalWrite(TESTMINI, LOW); 
+  //delay(1000);
 }
 
-//The interrup function for miniscope frame capturing
+///-------Interrup function for miniscope frame capturing and serial output-----------///
 void miniscopeFrame(){
-
-frameNumber++; //Add this frame to the number log
-timeStamp = now(); //Get a timestamp for the incoming frame
-if (!BpodFlag) {
-  Serial.println(frameNumber);
-  Serial.println(timeStamp);
- // Serial.write(sprintf('# %d,%d',frameNumber, timeStamp)
+  if (initialized){
+    frameNumber++; //Add this frame to the number log
+    timeStamp = millis(); //Get a timestamp for the incoming frame
+    Serial.printf("%d,%lu,%llu\n", MINISCOPE, frameNumber, timeStamp);
+  }
 }
-BpodFlag = 0; //Reset the Bpod flag to false
 
+///-----Interrupt function for Bpod trial event info------///
+void BpodTrialEvent(){
+  if (initialized){
+    timeStamp = millis(); //Get the time stamp for the trial event
+    trialStartFlag = trialStartFlag == 0; //Switch the state of the logic variable
+    if (trialStartFlag) {
+      trialNumber++; //If this is a trial start go to the next trial number
+      Serial.printf("%d,%lu,%llu\n",BPOD, trialNumber, timeStamp);
+    //} else {
+   //  Serial.printf("# %lu,%llu - trial_end: %lu\n",frameNumber, timeStamp, trialNumber);
+    }
+  }
+}
+
+///------Get instructions from syncMiniscopeToBpod.py---------///
+void serialEvent() {
+  serialMessage = Serial.read();
+
+  //Reset the trial variables when a serial input arrives
+  if (serialMessage == 49) {
+  frameNumber = 0;
+  timeStamp = 0;
+  trialNumber = 0;
+  trialStartFlag = 0;
+
+  //Write the pin specs to the header
+  Serial.printf("Reset frame and trial count. Miniscope channels is: %d and Bpod channel is: %d\n", MINISCOPE, BPOD);
+  initialized = 1;
+  } else if (serialMessage == 48) {
+    initialized = 0;
+  }
 }
--- MiniscopeTools/syncMiniscopeToBpod.py
+++ MiniscopeTools/syncMiniscopeToBpod.py
@@ -1,23 +1,72 @@
-
+import os
 import serial
-import time
+import serial.tools.list_ports
+from time import strftime,gmtime
+ 
+#%%-----Find available teensy and establish connection---------
+ports = serial.tools.list_ports.comports(include_links=False)
+for port in ports :
+    portsList = [port.device]
+  
+if (len(portsList) == 1):
+    teensyCOM = serial.Serial(portsList[0], 9600) #Establish connection at matching rate!
+    print("Connected to teensy on " + portsList[0])
+elif (len(portsList) > 1):
+    teensyPortID = input("Multiple COM ports are connected. Please specify the recording teensy: ")
+    teensyCOM = serial.Serial(teensyPortID, 9600) #Establish connection at matching rate!
+    print("Connected to teensy on " + teensyPortID)
+else:
+    print("No COM port available. Please check teensy connection")
+    
+#%%-----Create a file and write a header---------
+rootDirectory = "C:/Users/Anne/Documents/Data"
 
-c = serial.Serial('COM3', 9600)
+animalID = input("Please specify the animal name: ")
+fileTime = strftime("%Y%m%d_%H%M%S", gmtime())
 
-write_to_file_path = "C:/Users/Lukas Oesch/Documents/ChurchlandLab/Behavioral_cage_desing/testRunArduinoDaq/test.txt";
-output_file = open(write_to_file_path, "w+");
+#Find the respective directory
+logFileName = (animalID + "_" + fileTime + "_miniscope.mscopelog")
+logFileDirectory = os.path.join(rootDirectory, animalID, fileTime, "miniscope")
 
-while True:
-    signal = c.read()
-    #print(signal)
-    entry = signal.decode("utf-8")
-    output_file.write(entry);
-    print(entry)
-    print("running")
-    c.flushOutput()
-    time.sleep(0.2)
-c.close()
-output_file.close()
+#Check if directory exists and create otherwise
+if not os.path.exists(logFileDirectory):
+    os.makedirs(logFileDirectory)
+    
+logFilePath = os.path.join(logFileDirectory, logFileName)
+output_file = open(logFilePath, "w+")
+
+#%%------Write file header info---------
 
 
+#%%-----INTERMEDIATE: Let user start and stop synchronization-------------
+#input("Press enter to start the acquisition. Press any key and enter to stop")
+
+#%%----Acquire messages from teensy and write to file
+teensyCOM.write(str.encode('1')) #Send a byte (49) to reset the counting of frames and start interrupts
+
+#and trials.
+sporadicReport = 0; #Sporadically report that syncing is still on
+counter = 0;
+#goFlag = True
+try:
+    while True:
+        signal = teensyCOM.read()
+        entry = signal.decode("utf-8")
+        output_file.write(entry);
+        print(entry)
+        teensyCOM.reset_output_buffer()
+        #teensyCOM.flushOutput()
+        #teensyCOM.reset_input_buffer() #Make sure to clear the buffer for new data
+        
+        #counter = counter+1
+        #if counter == 10000:
+            #    sporadicReport = sporadicReport+1
+            #    print("Received", sporadicReport*counter, "individual bytes")
+            #    counter = 0
+except KeyboardInterrupt:
+    #---Stop acquisition and close the file-------------------------
+    #teensyCOM.write(str.encode('0')) #Send a byte (48) to stop the interrupts
+    output_file.close()
+    teensyCOM.close()
+    print("Synchronization ended")
 
