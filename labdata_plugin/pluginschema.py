Added a series of trial parameters to the chipmunk schema that were not yet tracked. Fixed extraction of experimenter and weighting metadata and insertion.

labdata_plugin/pluginschema.py
@@ -39,8 +39,20 @@ class Chipmunk(dj.Imported):
         rewarded_position                   : enum('left','right')
         stim_events = NULL                  : longblob   # time of the events
         stim_event_duration = NULL          : float      # Duration of individual flashes or clicks
+        min_isi = NULL                      : float      # The shortest allowed interval between flashes or clicks
+        is_poisson = NULL                   : tinyint    # 1 if intervals between flashes / clicks are random, 0 if there are only two intervals
         stim_brightness = NULL              : float      # Flash brightness
         stim_loudness = NULL                : float      # Click loudness
+        extra_stim_duration = NULL          : float      # Added time the stimulus train is playing after one full presentation
+        proportion_left = NULL              : float      # Proportion of left-side trials
+        anti_bias_strength = NULL           : float      # Proportion of trials where the anti-bias updates the trial sides
+        anti_bias_tau = NULL                : float      # Integration window of choice biases for anti-bias algorithm
+        pre_stim_delay_min = NULL           : float      # Minimal pre-stim delay duration
+        pre_stim_delay_max = NULL           : float      # Maximal pre-stim delay duration
+        pre_stim_delay_lambda = NULL        : float      # Shape parameter for the distribution from which to sample the pre-stim delays
+        pre_stim_delay = NULL               : float      # Sampled pre-stim delay
+        incorrect_choice_timeout = NULL     : float      # Timeout and tone duration after incorrect choice
+        early_withdrawal_timeout = NULL     : float      # Timeout and noise duration after early withdrawal
 
         """
 
@@ -58,6 +70,7 @@ class Chipmunk(dj.Imported):
         t_gocue = NULL               : float      # enter DemonWaitForWithdrawalFromCenter
         t_react = NULL               : float      # enter DemonWaitForResponse
         t_response = NULL            : float      # DemonWrongChoice or DemonReward
+        t_response_port_out = NULL   : float      # Removing the nose from the response port
         t_end                        : float      # FinishTrial [seconds]
         stim_duration                : float      # default 1second + extrastim duration
         
@@ -100,8 +113,8 @@ class Chipmunk(dj.Imported):
                         computer_name = None
             user_name = None
             if not metadata['experimenter'] is None:
-                if metadata['experimenter'] == 'HM': # fix hanna marsi label
-                    metadata['experimenter'] = 'Marsi'
+                if metadata['experimenter'] == 'HM': # fix hanna masri label
+                    metadata['experimenter'] = 'Masri'
                 elif metadata['experimenter'] == 'LY': # fix Letizia label
                     metadata['experimenter'] = 'Letizia'
                 elif metadata['experimenter'] == 'XL': # fix Xinyan label
@@ -143,7 +156,15 @@ class Chipmunk(dj.Imported):
             if not computer_name is None:
                 dset['setup_name'] = str(computer_name)
                 Dataset.update1(dset)
-                
+            
+            #Insert the weight
+            weight = metadata['weight']
+            if np.isfinite(weight):
+                Weighing.insert1(dict((Session & key).proj(
+                    weighing_datetime = 'session_datetime').fetch(
+                        'subject_name',
+                        'weighing_datetime',as_dict=True)[0],weight = weight))
+            
             if not len(trialdicts):
                 print(f'[chipmunk]: There are no trials for {key}')
                 return

labdata_plugin/utils.py
@@ -32,17 +32,23 @@ def load_chipmunk_trialdata(file_name):
         else:
             notes = ''
         tset = sesdata['TrialSettings'].tolist()
-        if 'rigNo' in  sesdata.dtype.fields:
+        if 'rigNo' in  tset.dtype.fields:
             setup_name = tset['rigNo'][0]
         else:
             setup_name = None
-        if 'researcher' in  sesdata.dtype.fields:
+        if 'researcher' in  tset.dtype.fields:
             experimenter = tset['researcher'][0]
         else:
             experimenter = None
+        if 'demonWeight' in tset.dtype.fields:
+            weight = tset['demonWeight'][0]
+        else:
+            weight = np.nan
+            
         metadata = dict(notes = notes,
                         setup_name = setup_name,
-                        experimenter = experimenter)
+                        experimenter = experimenter,
+                        weight = weight)
         
         tmp = sesdata['RawEvents'].tolist()
         tmp = tmp['Trial'].tolist()
@@ -114,9 +120,13 @@ def load_chipmunk_trialdata(file_name):
         event_times = []
         if 'StimulusDuration' in sesdata.dtype.fields:
             event_duration = sesdata['StimulusDuration'].tolist()
+        elif 'EventDuration' in sesdata.dtype.fields:
+            event_duration = sesdata['EventDuration'].tolist()
         else:
-            event_duration = [0.015]*trialdata.shape[0] #This value is hard-coded in chipmunk but can change in chipmunk_solo as of March 2026
+            event_duration = [None] * trialdata.shape[0]
+            #event_duration = [0.015]*trialdata.shape[0] #This value is hard-coded in chipmunk but can change in chipmunk_solo as of March 2026
         trialdata.insert(trialdata.shape[1], 'stimulus_event_duration', event_duration)
+        #TODO: Get minimum inter stimulus interval setting -> might not be explicitly stored with the chipmunk file
         
         for t in range(trialdata.shape[0]):
             if tmp_modality_numeric[t] < 3: #Unisensory
@@ -157,8 +167,42 @@ def load_chipmunk_trialdata(file_name):
         except Exception as err:
             print('[chipmunk]: For this version of chipmunk the task delays struct was not implemented yet.\nDid not generate the respective columns in the data frame.')
 
+        trialdata.insert(trialdata.shape[1], 'required_wait_time', sesdata['SetWaitTime'].tolist())                
         tmp = sesdata['ActualWaitTime'].tolist()
         trialdata.insert(trialdata.shape[1], 'actual_wait_time' , tmp)
+        trialdata.insert(trialdata.shape[1], 'pre_stim_delay', sesdata['PreStimDelay'].tolist())
+        if 'wrongPunishTimeout' in sesdata['TrialSettings'].tolist().dtype.fields:
+            trialdata.insert(trialdata.shape[1], 'incorrect_choice_timeout', sesdata['TrialSettings'].tolist()['wrongPunishTimeout'].tolist())
+        elif 'PunishDuration' in sesdata['TrialSettings'].tolist().dtype.fields:
+            trialdata.insert(trialdata.shape[1], 'incorrect_choice_timeout', sesdata['TrialSettings'].tolist()['PunishDuration'].tolist())
+        if 'earlyPunishTimeout' in sesdata['TrialSettings'].tolist().dtype.fields:
+            trialdata.insert(trialdata.shape[1], 'early_withdrawal_timeout', sesdata['TrialSettings'].tolist()['earlyPunishTimeout'].tolist())
+        elif 'EarlyPunishDuration' in sesdata['TrialSettings'].tolist().dtype.fields:
+            trialdata.insert(trialdata.shape[1], 'early_withdrawal_timeout', sesdata['TrialSettings'].tolist()['EarlyPunishDuration'].tolist())
+        if 'antiBiasStrength' in sesdata['TrialSettings'].tolist().dtype.fields:
+            trialdata.insert(trialdata.shape[1], 'anti_bias_strength', sesdata['TrialSettings'].tolist()['antiBiasStrength'].tolist())
+        elif 'UseAntiBias' in sesdata['TrialSettings'].tolist().dtype.fields:
+            trialdata.insert(trialdata.shape[1], 'anti_bias_strength', sesdata['TrialSettings'].tolist()['UseAntiBias'].tolist())
+        if 'antiBiasTau' in sesdata['TrialSettings'].tolist().dtype.fields:
+            trialdata.insert(trialdata.shape[1], 'anti_bias_tau', sesdata['TrialSettings'].tolist()['antiBiasTau'].tolist())
+        elif 'AntiBiasTau' in sesdata['TrialSettings'].tolist().dtype.fields:
+            trialdata.insert(trialdata.shape[1], 'anti_bias_tau', sesdata['TrialSettings'].tolist()['AntiBiasTau'].tolist())
+        if 'propLeft' in sesdata['TrialSettings'].tolist().dtype.fields:
+             trialdata.insert(trialdata.shape[1], 'proportion_left', sesdata['TrialSettings'].tolist()['propLeft'].tolist())
+        elif 'PropLeft' in sesdata['TrialSettings'].tolist().dtype.fields:
+             trialdata.insert(trialdata.shape[1], 'proportion_left', sesdata['TrialSettings'].tolist()['PropLeft'].tolist())
+        trialdata.insert(trialdata.shape[1], 'pre_stim_delay_min' ,sesdata['TrialSettings'].tolist()['preStimDelayMin'].tolist())
+        trialdata.insert(trialdata.shape[1], 'pre_stim_delay_max' ,sesdata['TrialSettings'].tolist()['preStimDelayMax'].tolist())
+        trialdata.insert(trialdata.shape[1], 'pre_stim_delay_lambda' ,sesdata['TrialSettings'].tolist()['preStimDelayLambda'].tolist())
+        if 'isPoissonStim' in sesdata['TrialSettings'].tolist().dtype.fields:
+             trialdata.insert(trialdata.shape[1], 'is_poisson', sesdata['TrialSettings'].tolist()['isPoissonStim'].tolist())
+        elif 'IsPoissonStim' in sesdata['TrialSettings'].tolist().dtype.fields:
+             trialdata.insert(trialdata.shape[1], 'is_poisson', sesdata['TrialSettings'].tolist()['IsPoissonStim'].tolist())
+        if 'minInterStimulusInterval' in sesdata.dtype.fields:
+            trialdata.insert(trialdata.shape[1], 'min_isi', sesdata['minimumInterStimulusInterval'])
+        else:
+            trialdata.insert(trialdata.shape[1], 'min_isi', [0.025]*trialdata.shape[0]) #This value is not generally stored in the .mat file but was unchanged
+        
         #TEMPORARY: import the demonstrator and observer id
         tmp = sesdata['TrialSettings'].tolist()
         if 'demonID' in tmp.dtype.fields:
@@ -347,7 +391,7 @@ def process_chipmunk_file(filepath):
     trial_settings_dict = []
     for itrial, trial in trialdata.iterrows():
         trialtimes = dict(trial_num = itrial,
-                          t_start = _get_state_time(trial,['WaitForCenterFixation','GoToCenter','DemonTrialStart']), 
+                          t_start = _get_state_time(trial,['WaitForCenterFixation','GoToCenter','DemonTrialStart', 'TrialStart']), 
                           t_sync = _get_state_time(trial,'Sync'),         
                           t_initiate = _get_state_time(trial,['InitFixation','DemonInitFixation']),
                           t_earlywithdraw = _get_state_time(trial,['EarlyWithdrawal','DemonEarlyWithdrawal']), 
@@ -355,6 +399,7 @@ def process_chipmunk_file(filepath):
                           t_gocue = _get_state_time(trial,['WaitForWithdrawalFromCenter','DemonWaitForWithdrawalFromCenter']),
                           t_react = _get_state_time(trial,['WaitForResponse','DemonWaitForResponse']),
                           t_response = _get_state_time(trial,['Reward','WrongChoice','DemonReward','DemonWrongChoice']),
+                          t_response_port_out = _get_state_time(trial, 'response_port_out'),
                           t_end = _get_state_time(trial,['FinishTrial','PrepareNextTrial']))
         if 'ExtraStimulusDuration' in trialdata.keys():
             stim_duration = 1. + trial['ExtraStimulusDuration']
@@ -405,7 +450,23 @@ def process_chipmunk_file(filepath):
                                         stim_rate_vision = trial.stimulus_rate_visual if np.isfinite(trial.stimulus_rate_visual) else None,
                                         category_boundary = trial.category_boundary,
                                         rewarded_position = 'right' if trial.correct_side == 1 else 'left',
-                                        stim_events = [f for f in trial.stimulus_event_timestamps]))
+                                        stim_events = [f for f in trial.stimulus_event_timestamps],
+                                        stim_event_duration = trial.stimulus_event_duration,
+                                        min_isi = trial.min_isi,
+                                        is_poisson = trial.is_poisson,
+                                        stim_brightness = trial.stimulus_brightness,
+                                        stim_loudness = trial.stimulus_loudness,
+                                        extra_stim_duration = trial.ExtraStimulusDuration,
+                                        proportion_left = trial.proportion_left,
+                                        anti_bias_strength = trial.anti_bias_strength,
+                                        anti_bias_tau = trial.anti_bias_tau,
+                                        pre_stim_delay_min = trial.pre_stim_delay_min,
+                                        pre_stim_delay_max= trial.pre_stim_delay_max,
+                                        pre_stim_delay_lambda = trial.pre_stim_delay_lambda,
+                                        pre_stim_delay = trial.pre_stim_delay,
+                                        required_wait_time = trial.required_wait_time,
+                                        incorrect_choice_timeout = trial.incorrect_choice_timeout,
+                                        early_withdrawal_timeout = trial.early_withdrawal_timeout))
     max_stims = np.max([len(np.unique([t['stim_rate_audio'] 
                                    for t in trial_settings_dict 
                                    if not t['stim_rate_audio'] is None])),