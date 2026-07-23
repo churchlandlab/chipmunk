d5d0a7d8d8f3dc2fd24f77a15e396f749693eebf
Gabriel Rojas Bowe <grojasbowe@gmail.com>
2026-07-22 17:40:18 -0700

    Register Chipmunk labdata dashboard

--- labdata_plugin/__init__.py
+++ labdata_plugin/__init__.py
@@ -1,2 +1,16 @@
-from .utils import load_chipmunk_trialdata, process_chipmunk_file, chipmunk_insert_decision_task
-from .pluginschema import Chipmunk,chipmunkschema
+from .pluginschema import Chipmunk, chipmunkschema
+from .utils import (
+    chipmunk_insert_decision_task,
+    load_chipmunk_trialdata,
+    process_chipmunk_file,
+)
+
+
+dashboard_name = "**Chipmunk**"
+
+
+def dashboard_function(schema=None):
+    """Render the Chipmunk page in the labdata Streamlit dashboard."""
+    from chipmunk_dashboard.streamlit_page import render_dashboard
+
+    return render_dashboard(schema=schema)
