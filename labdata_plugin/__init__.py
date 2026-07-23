1cc16a34bfa5557cb40de3210c6c5d6581806486
Gabriel Rojas Bowe <grojasbowe@gmail.com>
2026-07-23 16:56:13 -0700

    add feature for loading the chipmunk dashboard in labdata

--- labdata_plugin/__init__.py
+++ labdata_plugin/__init__.py
@@ -1,3 +1,5 @@
+from importlib.util import find_spec
+
 from .pluginschema import Chipmunk, chipmunkschema
 from .utils import (
     chipmunk_insert_decision_task,
@@ -6,11 +8,12 @@ from .utils import (
 )
 
 
-dashboard_name = "**Chipmunk**"
+if find_spec("chipmunk_dashboard") is not None:
+    dashboard_name = "**Chipmunk**"
 
 
-def dashboard_function(schema=None):
-    """Render the Chipmunk page in the labdata Streamlit dashboard."""
-    from chipmunk_dashboard.streamlit_page import render_dashboard
+    def dashboard_function(schema=None):
+        """Render the Chipmunk page in the labdata Streamlit dashboard."""
+        from chipmunk_dashboard.streamlit_page import render_dashboard
 
-    return render_dashboard(schema=schema)
+        return render_dashboard(schema=schema)
